8e671efddf48eefac099be877cc62c3942b26160
Jimmy Fang <jimmyfang23@gmail.com>
2023-12-08 23:53:19 -0500

    updated newstudentcontainer.js

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -9,10 +9,9 @@ import Header from './Header';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-
+import PropTypes from "prop-types";
 import NewStudentView from '../views/NewStudentView';
 import { addStudentThunk } from '../../store/thunks';
-
 class NewStudentContainer extends Component {
   // Initialize state
   constructor(props){
@@ -20,27 +19,54 @@ class NewStudentContainer extends Component {
     this.state = {
       firstname: "", 
       lastname: "", 
+      imageUrl: "",
       campusId: null, 
       redirect: false, 
       redirectId: null
     };
   }
 
+  async validateInput(event){
+    var letters = /^[A-Za-z]+$/;
+
+    if (event.target.name === "firstname")
+      if(!event.target.value.match(letters)){
+        alert("Invalid first name!");
+        return false;
+      }
+    else if (event.target.name === "lastname")
+      if(!event.target.value.match(letters)){
+        alert("Invalid last name!");
+        return false;
+      }
+    else if (event.target.name === "imageUrl") 
+      if(!event.target.value.match(/\.(jpeg|jpg|gif|png)$/)){
+        alert("Invalid image url!");
+        return false;
+      }
+    else
+      return true;
+    
+    
+  }
+
   // Capture input data when it is entered
   handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+    if(this.validateInput(event)){
+      this.setState({[event.target.name]: event.target.value})
+    }
   }
 
   // Take action after user click the submit button
   handleSubmit = async event => {
     event.preventDefault();  // Prevent browser reload/refresh after submit.
-
     let student = {
-        firstname: this.state.firstname,
-        lastname: this.state.lastname,
-        campusId: this.state.campusId
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      campusId: this.state.campusId,
+      email: this.state.email,
+      imageUrl: this.state.imageUrl === null ? "" : this.state.imageUrl,
+      gpa: this.state.gpa
     };
     
     // Add new student in back-end database
@@ -50,7 +76,9 @@ class NewStudentContainer extends Component {
     this.setState({
       firstname: "", 
       lastname: "", 
-      campusId: null, 
+      email: "",
+      imageUrl: "",
+      gpa: null,
       redirect: true, 
       redirectId: newStudent.id
     });
