d1db06bf72469693bdc4ff73f8e02626d013cd40
Vinson12090 <Vincentyan0718@gmail.com>
2023-12-08 22:50:39 -0500

    Changes to StudentView and Actions

---
+++ src/components/containers/EditStudentContainer.js
@@ -0,0 +1,143 @@
+/*==================================================
+EditStudentContainer.js
+
+The Container component is responsible for stateful logic and data fetching, and
+passes data (if any) as props to the corresponding View component.
+If needed, it also defines the component's "connect" function.
+================================================== */
+import Header from './Header';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { fetchStudentThunk, editStudentThunk } from "../../store/thunks";
+
+import EditStudentView from '../views/EditStudentView';
+
+class EditStudentContainer extends Component {
+  //Initialize state
+  constructor(props){
+    super(props);
+    this.state = {
+      id: null,
+      firstname: "", 
+      lastname: "", 
+      campusId: "",
+      email: "",
+      imageUrl: "",
+      gpa: null,
+      redirect: false,
+      redirectId: null,
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchStudent(this.props.match.params.id)
+
+  }
+
+  async validateInput(event){
+    var letters = /^[A-Za-z]+$/;
+
+    if (event.target.name === "firstname")
+      if(!event.target.value.match(letters)){
+        alert("Invalid first name!");
+        return false;
+      }
+    else if (event.target.name === "lastname")
+      if(!event.target.value.match(letters)){
+        alert("Invalid last name!");
+        return false;
+      }
+    else if (event.target.name === "imageUrl") 
+      if(!event.target.value.match(/\.(jpeg|jpg|gif|png)$/)){
+        alert("Invalid image url!");
+        return false;
+      }
+    else
+      return true;
+     
+  }
+
+  // Capture input data when it is entered
+  handleChange = event => {
+    if(this.validateInput(event)){
+      this.setState({[event.target.name]: event.target.value})
+    }
+  }
+
+  // Take action after user click the submit button
+  handleSubmit = async event => {
+    event.preventDefault();  // Prevent browser reload/refresh after submit.
+  
+
+    let student = {
+      id: this.props.student.id,
+      firstname: this.state.firstname === "" ? this.props.student.firstname : this.state.firstname,
+      lastname: this.state.lastname === "" ? this.props.student.lastname : this.state.lastname,
+      campusId: this.state.campusId === "" ? this.props.student.campusId : this.state.campusId,
+      email: this.state.email === "" ? this.props.student.email : this.state.email,
+      imageUrl: this.state.imageUrl === "" ? this.props.student.imageUrl : this.state.imageUrl,
+      gpa: this.state.gpa == null ? this.props.student.gpa : this.state.gpa,
+    };
+    // Edit student in back-end database
+    await this.props.editStudent(student);
+
+    // Update state, and trigger redirect to show the edited student
+    this.setState({
+      firstname: "", 
+      lastname: "", 
+      campusId: "",
+      email: "",
+      imageUrl: "",
+      gpa: null,
+      redirect: true, 
+      redirectId: this.props.student.id
+    });
+  }
+
+  // Unmount when the component is being removed from the DOM:
+  componentWillUnmount() {
+      this.setState({redirect: false, redirectId: null});
+  }
+
+  // Render new student input form
+  render() {
+    // Redirect to new student's page after submit
+    if(this.state.redirect) {
+      return (<Redirect to={`/student/${this.state.redirectId}`}/>)
+    }
+
+    // Display the input form via the corresponding View component
+    return (
+      <div>
+        <Header />
+        <EditStudentView 
+          handleChange={this.handleChange} 
+          handleSubmit={this.handleSubmit}
+          student={this.props.student}
+        />
+      </div>          
+    );
+  }
+}
+
+const mapState = (state) => {
+  return {
+    student: state.student,  // Get the State object from Reducer "student"
+  };
+};
+
+// The following input argument is passed to the "connect" function used by "EditStudentContainer" component to connect to Redux Store.
+// The "mapDispatch" argument is used to dispatch Action (Redux Thunk) to Redux Store.
+// The "mapDispatch" calls the specific Thunk to dispatch its action. The "dispatch" is a function of Redux Store.
+const mapDispatch = (dispatch) => {
+    return({
+        editStudent: (student) => dispatch(editStudentThunk(student)),
+        fetchStudent: (id) => dispatch(fetchStudentThunk(id)),
+    })
+}
+
+// Export store-connected container by default
+// EditStudentContainer uses "connect" function to connect to Redux Store and to read values from the Store 
+// (and re-read the values when the Store State updates).
+export default connect(mapState, mapDispatch)(EditStudentContainer);
--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,14 +4,38 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
-const StudentView = (props) => {
-  const { student } = props;
+import { Link } from "react-router-dom";
 
+const StudentView = (props) => 
+{
+  const {student, deleteStudent} = props;
   // Render a single Student view 
+  let campusDisplayMessage;
+  let studentImage;
+  // Render a single Campus view with list of its students
+  
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <img src={!student.imageURL ? "https://m.media-amazon.com/images/I/41OY4eExh+L._AC_.jpg":student.imageURL} height="500px" width="500" alt='campus image'></img>
+      <div>email: {student.email + " GPA: " + student.gpa }</div>
+      {student.campus.id}
+      <div key={student.campus.id}>
+        {
+          student.campus.id === "0" ? 
+          <h1>Not enrolled in any campus.</h1>
+          :
+          <h3><Link to={`/campus/${student.campus.id}`}>{student.campus.name}</Link></h3>
+        }
+
+      </div>
+      <Link to={`/edit-student/${student.id}`}>
+        <button>Edit</button>
+      </Link>
+      <Link to={'/students'}>
+      <button onClick={() => deleteStudent(student.id)}>Delete Student</button>
+      </Link>
+              
     </div>
   );
 
